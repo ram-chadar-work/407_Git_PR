5ff405f0451f4c64941d5cd4e9cdda820963cf77
ram-chadar <javaprojectwork2715@gmail.com>
2025-10-06 16:06:47 +0530

    ramchadar task

---
+++ src/main/java/RamChadar.java
@@ -0,0 +1,23 @@
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+import com.tka.config.HibernateConfig;
+import com.tka.entity.Employee;
+
+public class RamChadar {
+
+	public static void main(String[] args) {
+		SessionFactory sf = HibernateConfig.getSessionFactory();
+
+		Session session = sf.openSession();
+
+		Employee employee = new Employee("AA", 25);
+
+		Transaction tr = session.beginTransaction();
+		session.save(employee);
+		tr.commit();
+
+	}
+
+}
